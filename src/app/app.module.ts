9153734a10bd083fac46fbcf836147c8ccf19e69
RCOliveira98 <rcoliveira.ti@gmail.com>
2020-07-23 19:51:38 -0300

    [components and app] create loading component and update app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { WorldDataPanelComponent } from './components/world-data-panel/world-data-panel.component';
 import { CountryDataPanelComponent } from './components/country-data-panel/country-data-panel.component';
+import { LoadingComponent } from './components/loading/loading.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
     WorldDataPanelComponent,
-    CountryDataPanelComponent
+    CountryDataPanelComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
