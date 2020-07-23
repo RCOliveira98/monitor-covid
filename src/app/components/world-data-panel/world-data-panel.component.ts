f87bab397d127f678a3b9d2df855265d39ce056b
RCOliveira98 <rcoliveira.ti@gmail.com>
2020-07-23 19:52:22 -0300

    [components] world-data-panel: update

--- src/app/components/world-data-panel/world-data-panel.component.ts
+++ src/app/components/world-data-panel/world-data-panel.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { faExclamationTriangle, faCheck, faGlobeAmericas } from '@fortawesome/free-solid-svg-icons';
+
+import { World } from './../../shared/models/world.model';
+
+import { CovidService } from './../../services/covid.service';
+import { UtilsService } from './../../services/utils.service';
 
 @Component({
   selector: 'app-world-data-panel',
   templateUrl: './world-data-panel.component.html',
   styleUrls: ['./world-data-panel.component.css']
 })
-export class WorldDataPanelComponent implements OnInit {
 
-  constructor() { }
+export class WorldDataPanelComponent implements OnInit, OnDestroy {
+
+  public subscription: Subscription;
+  public world: World;
+  public renderComponent: boolean;
+  public alert = faExclamationTriangle;
+  public success = faCheck;
+  public globo = faGlobeAmericas;
+
+  constructor(
+    private serviceUtil: UtilsService,
+    private serviceCovid: CovidService
+  ) { }
 
   ngOnInit(): void {
+    this.renderComponent = false;
+    this.loadData();
+  }
+
+  ngOnDestroy(): void {
+    this.serviceUtil.unsubscribe(this.subscription);
+  }
+
+  loadData(): void {
+    this.serviceUtil.unsubscribe(this.subscription);
+    this.renderComponent = false;
+
+    this.subscription = this.serviceCovid.getWorldData().subscribe(
+      (data: World) => this.world = data,
+      e => console.error(e),
+      () => this.renderComponent = true
+      );
   }
 
 }
