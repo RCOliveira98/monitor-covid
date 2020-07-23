8e58266c80839d96f44a4ee83721095eb341f650
RCOliveira98 <rcoliveira.ti@gmail.com>
2020-07-23 19:49:53 -0300

    [services] covid-services: add methods getWorldData and getCountryData

--- src/app/services/covid.service.ts
+++ src/app/services/covid.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { World } from '../shared/models/world.model';
+import { Country } from '../shared/models/country.model';
+
+import { api } from '../shared/utils/url-api';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CovidService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  /**
+   * Informa dados da situação do covid-19 no cenário mundial.
+   *
+   * @returns {Observable<World>}
+   * @memberof CovidService
+   */
+  getWorldData(): Observable<World> {
+    return this.http.get<World>(`${api}/all`);
+  }
+
+  /**
+   * Informa dados da situação do covid-19 em um país específico.
+   *
+   * @param {string} [country='Brazil'] nome do país. Por padrão será o Brazil.
+   * @returns {Observable<Country>}
+   * @memberof CovidService
+   */
+  getCountryData(country: string = 'Brazil'): Observable<Country> {
+    return this.http.get<Country>(`${api}/countries/${country}`);
+  }
+
 }
