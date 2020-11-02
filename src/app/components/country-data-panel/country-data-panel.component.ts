21c0b3812004aae36df45856e363bc96440916bc
RCOliveira98 <rcoliveira.ti@gmail.com>
2020-11-02 20:54:26 -0300

    change country

--- src/app/components/country-data-panel/country-data-panel.component.ts
+++ src/app/components/country-data-panel/country-data-panel.component.ts
@@ -2,12 +2,15 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 
+import Swal from 'sweetalert2';
+
 import { faExclamationTriangle, faCheck, faGlobeAmericas } from '@fortawesome/free-solid-svg-icons';
 
 import { UtilsService } from './../../services/utils.service';
 import { CovidService } from './../../services/covid.service';
 
 import { Country } from './../../shared/models/country.model';
+
 
 @Component({
   selector: 'app-country-data-panel',
@@ -22,6 +25,7 @@ export class CountryDataPanelComponent implements OnInit, OnDestroy {
   public success = faCheck;
   public globo = faGlobeAmericas;
   private subscription: Subscription;
+  private subscriptionChangeCountry: Subscription;
 
   constructor(
     private serviceUtils: UtilsService,
@@ -33,17 +37,30 @@ export class CountryDataPanelComponent implements OnInit, OnDestroy {
     this.loadData();
   }
 
-  ngOnDestroy(): void { this.serviceUtils.unsubscribe(this.subscription); }
+  ngOnDestroy(): void {
+    this.serviceUtils.unsubscribe(this.subscription);
+    this.serviceUtils.unsubscribe(this.subscriptionChangeCountry);
+  }
 
-  loadData(): void {
-    this.subscription = this.serviceCovid.getCountryData().subscribe(
+  loadData(name: any = 'Brazil'): void {
+    this.subscription = this.serviceCovid.getCountryData(name).subscribe(
       (country: Country) => {
         this.country = country;
-        console.log(country);
       },
       e => console.error(e),
       () => this.renderComponent = true
     );
+  }
+
+  async changeCountry(){
+    const { value: nameCountry } = await Swal.fire({
+      title: 'Informe um País:',
+      input: 'text',
+      inputPlaceholder: 'País'
+    })
+    if (nameCountry) {
+      this.loadData(nameCountry);
+    }
   }
 
 }
